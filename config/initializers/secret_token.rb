1bcca5b9eee07e25cc46e0d9d920a8c16f0b29a3
persona <persona@magnus.lan>
2014-03-29 20:38:30 -0700

    repository initialization

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TestApp::Application.config.secret_key_base = '6a71e1ac14913a5b9f39dff6ba48f805f04a5386860ab77cc3adfc151ac6f8776b345523535165addc22e789f1c5068b0a79e9ee84f8afda06fa5c0097e922cb'
